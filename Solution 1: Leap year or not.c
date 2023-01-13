ef04cff16ad75e6f2a58398999919faf8bbd3d68
p3u? <121591269+p3uj@users.noreply.github.com>
2023-01-13 12:04:59 +0800

    Update Solution 1: Leap year or not.c

--- Solution 1: Leap year or not.c
+++ Solution 1: Leap year or not.c
@@ -7,7 +7,7 @@ main()
 
     printf("Enter the year: ");
     scanf("%d", &year);
-    //Using ?: operator to replace if else
+    //Using ternary operator to replace if else
     (year % 4 == 0) ? printf("%d is a Leap Year!", year) : printf("%d is not a Leap Year!",year);
     getch();
     return 0;
