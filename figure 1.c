eea2ee96d31e3bd672f6df809e46118c4f1a12b4
p3u? <121591269+p3uj@users.noreply.github.com>
2023-03-03 18:46:16 +0800

    Update figure 1.c

--- figure 1.c
+++ figure 1.c
@@ -1,11 +1,1 @@
-#include<stdio.h>
-int i, j;
-int main()
-{
-	for (i=1; i<=5; i++){       // It will loop as long as the value of i is less than or equal to 5 (Outer loop)
-        for (j=1; j<=i; j++)    // It will loop as long as the value of j is less than or equal to i (Inner loop)
-            printf("*");        // End
-        printf("\n");           //
-    }                           // End
-    return 0;
-}
+
