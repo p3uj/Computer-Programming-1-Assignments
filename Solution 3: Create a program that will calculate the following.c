676e362af8f579bb5237b92cce554b199bd0f9da
p3u? <121591269+p3uj@users.noreply.github.com>
2023-03-04 15:14:23 +0800

    Create Solution 3: Create a program that will calculate the following.c

---
+++ Solution 3: Create a program that will calculate the following.c
@@ -0,0 +1,23 @@
+// Using do while control structure
+#include<stdio.h>
+int day = 1, rate = 20, losesMiles = 2, totalMiles = 200;
+int miles;
+int main()
+{
+    printf("DAY\t\tRATE\t\tMILES\n");
+    if (day == 1){
+        miles = 50 + rate - losesMiles;     // 50 is the initial miles.
+        printf(" %d\t\t %d\t\t %d\n", day, rate, miles);
+    }
+    do                                                      // Do this conditon as long as the miles is not equal to total miles.
+    {   day++;                                              //
+        rate--;     // Rate decrease by 1                   //
+        miles = miles + rate - losesMiles;                  //
+        printf(" %d\t\t %d\t\t %d\n", day, rate, miles);    //
+    } while (miles != totalMiles);                          // End
+    if (day == 13)
+        printf("Condolence Bareng :(\nBareng dies on day %d", day);
+    else
+        printf("\nCongratulations Bareng!\nBareng reaches civilization on day %d!", day);
+    return 0; 
+}
